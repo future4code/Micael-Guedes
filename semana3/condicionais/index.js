0c1e0c23be1c5957c4fba326b391cec29f2d4e9a
Micael <mikeguedes@gmail.com>
2020-07-29 13:38:38 -0600

    exercicio semana3-aula11

---
+++ semana3/condicionais/index.js
@@ -0,0 +1,24 @@
+// ============================= EXERCÍCIO 1 =============================
+
+//Leia o código abaixo:
+
+
+const respostaDoUsuario = prompt("Digite o número que você quer testar")
+const numero = Number(respostaDoUsuario)
+
+if(numero % 2 === 0) {
+  console.log("Passou no teste.")
+} else {
+  console.log("Não passou no teste.")
+}
+
+//Explique o que o código faz. Qual o teste que ele realiza? Para que tipos de números ele imprime no console "Passou no teste"? Para que tipos de números a mensagem é "Não passou no teste"?
+
+// RESPOSTA: 
+// O código testa se números inseriados pelo usuário no prompt é impar ou par. 
+// "Passou no teste" é imprimido quando usuário insere número par.
+// "Não passou no teste" é imprimido quando usuário insere número ímpar.
+
+
+// ============================= EXERCÍCIO 2 =============================
+
